dc25645c02a78cc1ba97d494bdf054886333381c
PaulAduGyamfi <pjadu7@gmail.com>
2026-09-01 12:54:10 -0400

    linked list pop

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -10,7 +10,7 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def append(self, value):
+    def append(self, value) -> bool:
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -20,6 +20,21 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self) -> Node | None:
+        if self.length == 0:
+            return None
+        prev, curr = self.head, self.head
+        while(curr.next is not None):
+            prev = curr
+            curr = curr.next
+        self.tail = prev
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return curr
 
     def prepend(self, value):
         return
@@ -28,18 +43,14 @@ class LinkedList:
         return
 
     def print_list(self):
-        print("Head: ", my_linked_list.head.value)
-        print("Tail: ", self.tail.value)
-        print("Length: ", self.length)
-        print("\nLinked List:")
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
 my_linked_list = LinkedList(1)
-my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
+my_linked_list.pop()
 my_linked_list.print_list()
